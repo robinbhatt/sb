ced50a81fc41150010048c68afd68797dadde812
Robin <robybhatt@gmail.com>
2021-12-07 08:36:09 +0530

    fourth

---
+++ src/app/contactus/contactus.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  template: `
+    <p>
+      contactus works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContactusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
